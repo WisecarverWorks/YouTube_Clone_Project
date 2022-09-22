f64a25656887583214895aeffde0ddeca9e33deb
Kyle Wisecarver <wiseworks.2022@gmail.com>
2022-09-22 06:07:30 -0500

    docs: SearchBar.js complete. Searchbar css complete and imported into js file.

--- clone/frontend/src/components/SearchBar/SearchBar.js
+++ clone/frontend/src/components/SearchBar/SearchBar.js
@@ -1,7 +1,36 @@
 // General
+import React, { useState } from "react";
 // Component 
 // Hook
 // Context 
 // CSS 
+import "./SearchBar.css";
 // Utils
 // Pages 
+
+const SearchBar = ({ handleSearch }) => {
+    const [search, setSearch] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        handleSearch(search);
+        setSearch("");
+    };
+    return (
+        <form className="searchBar" onSubmit={handleSubmit}>
+            <label> Search For Videos{""}</label>
+            <br />
+            <input
+                type="text"
+                name="search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <br></br>
+            <br></br>
+            <button type="submit">Search!</button>
+        </form>
+    );
+};
+
+export default SearchBar;
