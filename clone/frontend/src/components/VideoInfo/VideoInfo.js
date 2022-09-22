08f70cd49ff21fa05803fd6dae7b21a66a551a57
Kyle Wisecarver <wiseworks.2022@gmail.com>
2022-09-22 06:08:18 -0500

    docs: Component VideoInfo created.

--- clone/frontend/src/components/VideoInfo/VideoInfo.js
+++ clone/frontend/src/components/VideoInfo/VideoInfo.js
@@ -5,3 +5,15 @@
 // CSS 
 // Utils
 // Pages 
+const VideoInfo = ({ title, description }) => {
+    return (
+        <>
+            <h2>{title}</h2>
+            <h3>
+                {description.length < 100 ? description : description.slice(0, 100)}
+            </h3>
+        </>
+    );
+};
+
+export default VideoInfo;
