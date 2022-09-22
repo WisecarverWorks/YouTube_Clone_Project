71a6c2e7d85a76477231ca724dbdabb36947c15c
Kyle Wisecarver <wiseworks.2022@gmail.com>
2022-09-22 11:24:30 -0500

    Changed names after reviewing starter and final code.

--- clone/YouTubeClone/backend/comments/urls.py
+++ clone/YouTubeClone/backend/comments/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from comments import views
 
-# <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
 urlpatterns = [
     path('', views.create_comment),
