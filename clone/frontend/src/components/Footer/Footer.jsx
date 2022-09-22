686e68f7024caa68885f61177638f1e3db5b9b14
Kyle Wisecarver <wiseworks.2022@gmail.com>
2022-09-22 06:02:06 -0500

    docs: Footer css and Footer component finished.

--- clone/frontend/src/components/Footer/Footer.jsx
+++ clone/frontend/src/components/Footer/Footer.jsx
@@ -3,5 +3,16 @@
 // Hook
 // Context 
 // CSS 
+import "./Footer.css";
 // Utils
 // Pages 
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright © 2022</p>{" "}
+        </footer>
+    );
+};
+
+export default Footer;
